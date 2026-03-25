12b6a151194f648cb39f4de75cfbb56fbfd30461
Cleverton Neves <clever.nvs@gmail.com>
2026-03-25 16:28:55 -0300

    feat: Add role-based access control with Role decorator and RolesGuard

--- apps/api/src/users/users.controller.ts
+++ apps/api/src/users/users.controller.ts
@@ -1,16 +1,20 @@
-import { Controller, Post, Body, ValidationPipe } from '@nestjs/common';
+import { Controller, Post, Body, ValidationPipe, UseGuards } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UsersService } from './users.service';
 import { ReturnUserDto } from './dto/return-user.dto';
+import { AuthGuard } from '@nestjs/passport';
+import { RolesGuard } from '../auth/roles.guard';
+import { Role } from '../auth/role.decorator';
+import { UserRole } from './user-roles.enum';
 
 @Controller('users')
 export class UsersController {
     constructor(private usersService: UsersService) { }
 
     @Post()
-    async createAdminUser(
-        @Body(ValidationPipe) createUserDto: CreateUserDto,
-    ): Promise<ReturnUserDto> {
+    @Role(UserRole.ADMIN)
+    @UseGuards(AuthGuard(), RolesGuard)
+    async createAdminUser(@Body(ValidationPipe) createUserDto: CreateUserDto): Promise<ReturnUserDto> {
         const user = await this.usersService.createAdminUser(createUserDto);
         return {
             user,
